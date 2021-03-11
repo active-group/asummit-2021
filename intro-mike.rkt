40f6b20f05d3a01f22438490dfd886362981f8d3
Mike Sperber <sperber@deinprogramm.de>
2021-03-11 10:24:02 +0100

    Update.

--- intro-mike.rkt
+++ intro-mike.rkt
@@ -2,3 +2,9 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname intro-mike) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
 (define x (+ 12 23))
+
+(define circle1 (circle 50 "solid" "gold"))
+
+(define square1 (square 100 "outline" "green"))
+
+(define star1 (star 50 "solid" "blue"))
