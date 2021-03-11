ed8c732baff4465442cb55eb88013f4ac91d564a
Mike Sperber <sperber@deinprogramm.de>
2021-03-11 14:47:00 +0100

    Update.

--- haskell-code/IntroMike.hs
+++ haskell-code/IntroMike.hs
@@ -38,6 +38,6 @@ dillo1 = Dillo Alive 10 -- lebendiges Gürteltier, 10kg
 dillo2 = Dillo Dead 8 -- totes Gürteltier, 8kg
 
 -- Gürteltier überfahren
--- runOverDillo :: Dillo -> Dillo
+runOverDillo :: Dillo -> Dillo
 runOverDillo (Dillo l w) = Dillo Dead w
 --           ^^^^^^^^^^^ Pattern
