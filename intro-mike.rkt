ab8be6bb7c6b156df36dd5860ccdfe12b0623029
Mike Sperber <sperber@deinprogramm.de>
2021-03-11 11:41:17 +0100

    Update.

--- intro-mike.rkt
+++ intro-mike.rkt
@@ -142,8 +142,19 @@
 (define dillo1 (make-dillo #t 10)) ; lebendiges Gürteltier, 10kg
 (define dillo2 (make-dillo #f 8)) ; totes Gürteltier, 8kg
 
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
 
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2
+              #;(make-dillo #f 8))
 
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
+    
 #|
 
 In Java/OO-Sprachen:
