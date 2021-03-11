a1eb82035ed0c4a638d2dceb301afab4c9810b0f
Mike Sperber <sperber@deinprogramm.de>
2021-03-11 12:03:14 +0100

    Update.

--- intro-mike.rkt
+++ intro-mike.rkt
@@ -202,8 +202,8 @@
 (define run-over-animal
   (lambda (animal)
     (cond
-      (... ...)
-      (... ...))))
+      ((dillo? animal) (run-over-dillo animal))
+      ((parrot? animal) (run-over-parrot animal)))))
 #|
 
 In Java/OO-Sprachen:
