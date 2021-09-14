4381cb3401b8f25a062c5fb18cddb5488329cfc7
Mike Sperber <sperber@deinprogramm.de>
2021-09-14 15:23:54 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -68,5 +68,5 @@ listSum [] = 0
 listSum (first:rest) = first + (listSum rest)
 
 listProduct :: [Int] -> Int
-listPropduct []
+listPropduct [] = 1
 listProduct (first:rest) = first * (listProduct rest)
