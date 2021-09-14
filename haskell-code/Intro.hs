3ef334ad03ab68587bc5d31fdbbef8b7afdd031e
Mike Sperber <sperber@deinprogramm.de>
2021-09-14 14:51:09 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -12,3 +12,13 @@ data Animal =
     Dillo Bool Int 
   | Parrot String Int
   deriving Show
+
+-- Gürteltier, lebendig, 10kg
+dillo1 :: Animal
+dillo1 = Dillo True 10
+-- totes Gürteltier, 12kg
+dillo2 :: Animal
+dillo2 = Dillo False 12
+
+parrot1 :: Animal
+parrot1 = Parrot "Hallo1" 1
