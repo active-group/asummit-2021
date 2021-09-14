7055395c5dc93fed3ed536af48bd2d42061f9d8f
Mike Sperber <sperber@deinprogramm.de>
2021-09-14 10:39:55 +0200

    Update.

--- image.rkt
+++ image.rkt
@@ -1,5 +1,10 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname image) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-(define x (+ 12 23))
+(define x
+  (+ 12
+     (* 23 42)))
+
+(define star1 (star 50 "solid" "green"))
+(define circle1 (circle 50 "solid" "red"))
 
