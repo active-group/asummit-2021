a964fd51407f75db7ef537b906e783f8ba24489c
Mike Sperber <sperber@deinprogramm.de>
2021-09-14 14:59:57 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -1,5 +1,6 @@
 module Intro where
 
+import System.IO.Error (permissionErrorType)
 x :: Integer
 x = 12 * 23
 
@@ -8,17 +9,30 @@ x = 12 * 23
 -- - Papagei (Satz, Gewicht)
 -- gemischte Daten aus zusammengesetzte Daten
 -- ---> algebraische Datentyp
+data Liveness = Dead | Alive
+  deriving Show
+
+-- Typ-Synonym
+type Weight = Int
+
 data Animal =
-    Dillo Bool Int 
-  | Parrot String Int
+    Dillo Liveness Weight
+  | Parrot String Weight
   deriving Show
 
 -- Gürteltier, lebendig, 10kg
 dillo1 :: Animal
-dillo1 = Dillo True 10
+dillo1 = Dillo Alive 10
 -- totes Gürteltier, 12kg
 dillo2 :: Animal
-dillo2 = Dillo False 12
+dillo2 = Dillo Dead 12
 
 parrot1 :: Animal
 parrot1 = Parrot "Hallo1" 1
+
+-- Tier überfahren
+runOverAnimal :: Animal -> Animal
+-- 1 Gleichung pro Fall im Datentyp
+runOverAnimal (Dillo liveness w) = Dillo Dead w
+runOverAnimal (Parrot _ weight) = Parrot "" weight
+
