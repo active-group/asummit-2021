183e49b3943eca410a6f1f2ea84d9f3eae37354d
Mike Sperber <sperber@deinprogramm.de>
2021-09-14 14:48:48 +0200

    Update.

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -2,3 +2,12 @@ module Intro where
 
 x :: Integer
 x = 12 * 23
+
+-- Ein Tier auf dem texanischen Highway ist eins der folgenden:
+-- - Gürteltier (lebendig/tot, Gewicht)
+-- - Papagei (Satz, Gewicht)
+-- gemischte Daten aus zusammengesetzte Daten
+-- ---> algebraische Datentyp
+data Animal =
+    Dillo Bool Int 
+  | Parrot String Int
