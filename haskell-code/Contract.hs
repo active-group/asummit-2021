52af3389c488660554471c62b01b84bf5087b52c
Mike Sperber <sperber@deinprogramm.de>
2021-09-15 11:56:44 +0200

    Update.

--- haskell-code/Contract.hs
+++ haskell-code/Contract.hs
@@ -1,2 +1,14 @@
 module Contract where
 
+-- Zero-Coupon-Bond 
+-- Ich bekomme Weihnachten 100 EUR.
+-- Ich bekomme Weihnachten 100 GBP.
+
+import Prelude (Show)
+type Date = String -- "2021-12-24"
+type Amount = Double
+data Currency = EUR | GBP
+
+data Contract =
+    ZeroCouponBond Date Amount Currency 
+    deriving Show
