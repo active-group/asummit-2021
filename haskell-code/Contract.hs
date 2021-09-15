4938ae5424b1480ffe6fd9f990cf24b1b10237f0
Mike Sperber <sperber@deinprogramm.de>
2021-09-15 12:11:36 +0200

    Update.

--- haskell-code/Contract.hs
+++ haskell-code/Contract.hs
@@ -9,9 +9,24 @@ type Amount = Double
 data Currency = EUR | GBP
   deriving Show
 
+-- Funktionale Modellierung / Modellierung mit Kombinatoren
+-- 1. möglichst einfache Beispiele
+-- 2. in möglichst kleine Einzelteile / Ideen zerlegen
+
 -- Alternative:
 -- data Amount = Amount Double Currency
 
+-- Zero-Coupon Bond:
+-- 1. Idee: Währung festlegen / "Währung"
+-- 2. Idee: Betrag
+-- 3. Idee: "später"
+
+data Contract =
+    Coin Currency -- "1 EUR jetzt"
+
+
+
+{-
 data Contract =
      ZeroCouponBond Date Amount Currency 
    | CurrencySwap Date Amount Currency Amount Currency
@@ -33,3 +48,5 @@ zcb2 :: Contract
 zcb2 = ZeroCouponBond "2021-12-24" 100.0 GBP
 
 swap1 = CurrencySwap "2021-12-24" 100 EUR 100 GBP
+
+-}
